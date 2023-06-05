3723feac8579a8ed03b2934e72df3f717bb86875
Leon <tahjib.leon399@gmail.com>
2023-06-05 07:14:28 +0600

    testing

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,16 @@
-import express, { Application, Response, Request } from 'express';
-import cors from 'cors';
+import express, { Application, Response, Request } from 'express'
+import cors from 'cors'
 
-const app : Application = express()
-const port = 3000
+const app: Application = express()
 
-app.use(cors());
+         app.use(cors())
 
 // parser
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (req: Request, res: any) => {
   res.send('Working succesfully')
 })
 
-export default app;
+export default app
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,18 @@
-import mongoose from 'mongoose';
-import app from './app';
-import config from './config/index';
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config/index'
 
 async function boostrap() {
-    try {
-        await mongoose.connect(config.database_url as string);
-        console.log("Database connected succesfully");
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log('Database connected succesfully')
 
-        app.listen(config.port, () => {
-            console.log(`Application listening on port ${config.port}`)
-        })
-
-    } catch (error) {
-        console.log(error);
-    }
+    app.listen(config.port, () => {
+      console.log(`Application listening on port ${config.port}`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
 }
 
-boostrap();
+boostrap()
